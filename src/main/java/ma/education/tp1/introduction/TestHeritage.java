b20f3ae65f4f61565e182abffb180f896e81ee73
USER <abarkansalim@gmail.com>
2022-02-25 17:22:35 +0100

    'Séance 2' (6.)

--- src/main/java/ma/education/tp1/introduction/TestHeritage.java
+++ src/main/java/ma/education/tp1/introduction/TestHeritage.java
@@ -4,7 +4,7 @@ public class TestHeritage {
     public static void main(String[] args) {
         Salle s1=new SalleCours(1, "Salle 1", (byte) 20);
         SalleCours s2= new SalleCours(2, "Salle 2", (byte) 20);
-        // SalleCours s3=s1; on peu pas affecter class mere a class fille sans cast
+         SalleCours s3= (SalleCours) s1;
         SalleCours s4=s2;
     }
 }

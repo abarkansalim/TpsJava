a2631d1724a4669fe8ca1cdb63b4b74e4157e57b
USER <abarkansalim@gmail.com>
2022-02-25 19:15:43 +0100

    'Séance 3 I.' (3. 4.)

--- src/main/java/ma/education/tp2/statickeyword/Etudiant.java
+++ src/main/java/ma/education/tp2/statickeyword/Etudiant.java
@@ -1,7 +1,7 @@
 package ma.education.tp2.statickeyword;
 
 public class Etudiant {
-    long id;
-    String nom;
-    int nbEtudiants;
+    public long id; //Oui
+    public String nom; //Oui
+    public static int nbEtudiants; //Non
 }

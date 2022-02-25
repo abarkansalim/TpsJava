31a45207ebe329387b0615befcd8f4fc254cec37
USER <abarkansalim@gmail.com>
2022-02-25 17:26:01 +0100

    'Séance 2' (7. 8.)

--- src/main/java/ma/education/tp1/introduction/TestHeritage.java
+++ src/main/java/ma/education/tp1/introduction/TestHeritage.java
@@ -4,7 +4,12 @@ public class TestHeritage {
     public static void main(String[] args) {
         Salle s1=new SalleCours(1, "Salle 1", (byte) 20);
         SalleCours s2= new SalleCours(2, "Salle 2", (byte) 20);
-         SalleCours s3= (SalleCours) s1;
+        SalleCours s3= (SalleCours) s1;
         SalleCours s4=s2;
+
+        Salle s5=new Laboratoire(2, "LABO", "CHIMIE");
+        SalleCours s6= new SalleCours(2, "Salle 2", (byte) 20);
+        SalleCours s7=s5; //on peut pas instancier un classe mere d'une class fille
+        SalleCours s8=s6;
     }
 }

e043ff3485acdf88332e8a88bb9abfa78b1e075d
USER <abarkansalim@gmail.com>
2022-03-01 15:41:35 +0100

    'Séance 6 I.' (1. 2. 3. 4. 5. 6.)

---
+++ src/main/java/ma/education/tp6/annotations/TestReflectionAnnotation.java
@@ -0,0 +1,9 @@
+package ma.education.tp6.annotations;
+
+public class TestReflectionAnnotation {
+    public static void main(String[] args) {
+        Class c = Calculatrice.class;
+        Programmer programmer = (Programmer)c.getDeclaredAnnotation(Programmer.class);
+        System.out.println(programmer.id()+":"+programmer.name());
+    }
+}
